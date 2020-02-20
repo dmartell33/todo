b9e47251c2167116e0ae686027103cd507068ef3
Dedrick Harrington <dedrick33@live.com>
2020-02-20 21:05:53 +0000

    Created API

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe TasksController, type: :controller do
       expect(response_ids).to eq([task1.id, task2.id])
     end
   end
-  describe "tasks#update" do
-    it "should allow tasks to be marked as done" do
-      task = FactoryBot.create(:task, done: false)
-      put :update, params: {id: task.id, task: { done: true }}
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
       expect(response).to have_http_status(:success)
-      task.reload
-      expect(task.done).to eq(true)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
     end
   end
 
