28c4302e78abdcb6ab5dafc960f2c325f082d5db
Dedrick Harrington <dedrick33@live.com>
2020-06-02 03:11:45 +0000

    Toggle

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -39,18 +39,7 @@ $(function() {
     } );
     }
 
-    $.get("/tasks").success( function( data ) {
-      var htmlString = "";
-
-      $.each(data, function(index,  task) {
-        htmlString += taskHtml(task);
-      });
-      var ulTodos = $('.todo-list');
-      ulTodos.html(htmlString);
-
-      $('.toggle').change(toggleTask);
-
-    });
+    
 
 
     $('#new-form').submit(function(event) {
